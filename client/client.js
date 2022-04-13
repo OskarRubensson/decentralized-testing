522d72b961ed0634d4d1554368a676eb1a59736d
OskarRubensson <oskar231@hotmail.com>
2022-04-13 08:38:23 +0200

    Changed server ip-address

--- client/client.js
+++ client/client.js
@@ -1,5 +1,5 @@
 const io = require("socket.io-client");
-let socketClient = io("http://localhost:8000");
+let socketClient = io("https://decentralized-testing-server.herokuapp.com");
 let runTest = require("./test");
 
 socketClient.on("connect", () => {
--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const http = require('http');
+const http = require('https');
 const writeJson = require('./fileWriter');
 
 const app = express();
@@ -63,4 +63,4 @@ function appendTime(time) {
   }
 }
 
-server.listen(process.env.PORT || 8000, () => console.log(`server is running on port ${process.env.PORT || 8000}`));
+server.listen(process.env.PORT || 8000, "0.0.0.0", () => console.log(`server is running on port ${process.env.PORT || 8000}`));
