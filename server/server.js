9d510737abf3883289184c7186acfc78b2fe8bef
OskarRubensson <oskar231@hotmail.com>
2022-04-13 08:51:46 +0200

    Trying to get heroku to work

--- server/server.js
+++ server/server.js
@@ -63,4 +63,4 @@ function appendTime(time) {
   }
 }
 
-server.listen(process.env.PORT || 8000, "0.0.0.0", () => console.log(`server is running on port ${process.env.PORT || 8000}`));
+server.listen(process.env.PORT || 8000, () => console.log(`server is running on port ${process.env.PORT || 8000}`));
