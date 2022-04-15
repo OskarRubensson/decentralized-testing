6015ef37b93fa54875aa71dc3a6a600b5a929333
OskarRubensson <oskar231@hotmail.com>
2022-04-15 09:27:21 +0200

    Updated ec2 startup-script

--- install-ec2.sh
+++ install-ec2.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+################ This is the script that is run when the EC2-instances are launched ################
+
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
 source ~/.bashrc
@@ -7,17 +9,13 @@ sudo yum -y update
 sudo yum -y install git
 git clone https://github.com/OskarRubensson/decentralized-testing.git
 cd decentralized-testing
-npm install
 npm install -g hyper-gateway
+hyper-gateway run --silent &
 
-cd /tmp/
-sudo yum -y install libX11
-sudo curl https://intoli.com/install-google-chrome.sh | bash
-sudo mv /usr/bin/google-chrome-stable /usr/bin/google-chrome
-sudo wget https://chromedriver.storage.googleapis.com/100.0.4896.20/chromedriver_linux64.zip
-sudo unzip chromedriver_linux64.zip
-sudo mv chromedriver /usr/bin/chromedriver
 
-cd ~/decentralized-testing/
-hyper-gateway run --persist=false --silent &
-npm run client
+# install most recent package
+sudo amazon-linux-extras install docker -y
+# start the service docker
+sudo service docker start
+sudo docker build . -t="selenium"
+sudo docker run --name selenium-chrome --network="host" --shm-size=5g selenium
