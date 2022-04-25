d6a02781dc5e522d8dbcf5a66d968e1d10d48ac4
OskarRubensson <oskar231@hotmail.com>
2022-04-25 15:13:45 +0200

    preparing to make docker/ec2 for leechers and seeders

--- ec2-seeders.sh
+++ ec2-seeders.sh
@@ -2,28 +2,6 @@
 exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 ################ This is the script that is run when the EC2-instances are launched ################
 
-# Install Node, NVM, and NPM
-yum -y update
-
-# START
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-
-# Install NVM, NPM, Node.JS & Grunt
-nvm alias default 17
-nvm install 17
-nvm use 17
-
-# Clone git-repo
-sudo yum -y install git
-cd /home/ec2-user/
-git clone https://github.com/OskarRubensson/decentralized-testing.git
-cd decentralized-testing
-npm install -g hyper-gateway
-hyper-gateway run --silent &
-
-
 # install most recent docker version
 sudo amazon-linux-extras install docker -y
 sudo service docker start
